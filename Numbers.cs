bb1b133e2538f4ec732dae1050619d7c77359876
xxxZino <101564172+xxxZino@users.noreply.github.com>
2022-04-07 19:47:46 +0700

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas4
+{
+   public class Numbers
+    {
+        private int _a,_b;
+        public int FindMinimum(int number1,int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+        
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            _a = number1 < number3 ? number1 : (int)number3;
+            _b = number2 < number3 ? number2 : (int)number3;
+            return number1 < number2 ? _a : _b;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            _a = number1 > number3 ? number1 : (int)number3;
+            _b = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? _a : _b;
+        }
+    }
+}
